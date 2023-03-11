fa2ab0ecbcab187d723df0be65bf5afface34729
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-03-12 00:25:48 +0300

    Update 01_card_to_str.py

--- Module4/practice/deck/01_card_to_str.py
+++ Module4/practice/deck/01_card_to_str.py
@@ -6,7 +6,8 @@ class Card:
 
     def to_str(self):
         # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        suit_icons = {"Spades": '\u2660', "Clubs": '\u2663', "Diamonds": '\u2666', "Hearts": '\u2665'}
+        return f"{self.value}{suit_icons[self.suit]}"
 
 
 # Создадим несколько карт
