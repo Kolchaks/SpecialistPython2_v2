c7fbcaf194f14f9435c3b5868129b829a1bfc1ce
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-03-19 23:47:51 +0300

    Update deck_total.py

--- Module4/practice/deck/deck_tasks/deck_total.py
+++ Module4/practice/deck/deck_tasks/deck_total.py
@@ -7,7 +7,6 @@ class Card:
         self.suit = suit  # Масть карты
 
     def __str__(self) -> str:
-
         suit_icons = {"Spades": '\u2660', "Clubs": '\u2663', "Diamonds": '\u2666', "Hearts": '\u2665'}
         return f"{self.value}{suit_icons[self.suit]}"
 
@@ -17,7 +16,7 @@ class Card:
         """
         return self.suit == other_card.suit
 
-    def __gt__(self, other_card) -> bool:
+    def __gt__(self, other_card) -> bool:  # gt -> great than
 
         value_volume = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
                         '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
@@ -28,11 +27,16 @@ class Card:
         else:
             return value_volume[self.value] > value_volume[other_card.value]
 
-    def __lt__(self, other_card):
+    def __lt__(self, other_card):  # lt -> less than
         return not self.__gt__(other_card)
 
 
 class Deck:
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+    DIAMONDS = "Diamonds"
+    HEARTS = "Hearts"
+
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -42,6 +46,44 @@ class Deck:
             for value in values:
                 self.cards.append(Card(value, suit))
 
+    def __str__(self) -> str:
+        str_cards = []
+        for card in self.cards:
+            str_cards.append(str(card))
+
+        return f"cards[{len(self.cards)}]: " + ", ".join(str_cards)
+
+    def draw(self, number_of_cards: int) -> list:
+        """
+        Сдаёт определённое число карт на "руку"
+        """
+        hand, self.cards = self.cards[:number_of_cards], self.cards[number_of_cards:]
+        return hand
+
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
+
+    def shuffle(self) -> None:
+        """
+        Перемешивает колоду карт
+        """
+        return random.shuffle(self.cards)
+
+
+    def shift(self, num_card: int) -> None:
+        """
+        Сдвигает определённое число карт сверху и перемещает вниз колоды
+        """
+        self.cards = self.cards[num_card:] + self.cards[:num_card]
+
+
 if __name__ == "__main__":
+    card_1 = Card(10, Deck.DIAMONDS)
+    card_2 = Card("Q", Deck.HEARTS)
+    print(card_1, card_2, 'Привет!')
+
+# if __name__ == "__main__":
     # Тут можно разместить тесты классов
     ...
