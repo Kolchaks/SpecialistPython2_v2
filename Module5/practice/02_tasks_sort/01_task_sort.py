47cc0b10f3fa073409d3bac96fcdbd18d9af87d7
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-03-29 00:39:14 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -5,4 +5,4 @@
 numbers = [2.5, 13.6, 13, -22.4, -12.8, 6.7, 12.8, 21, 4, -22.1]
 # Задайте самостоятельно, выбрав произвольное число
 a = 13
-print(sum([i for i in numbers if i > a]))
+print(sum([item for item in numbers if i > a]))
